cbe31068419f59b1c61a2f0fb1c25101ac862c3f
Ethan <ethanbinya@aol.com>
2017-10-23 12:51:02 -0400

    Added methods.

--- src/com/company/Table.java
+++ src/com/company/Table.java
@@ -7,6 +7,10 @@ import java.util.List;
  * Created by Ethan on 10/17/2017.
  */
 public class Table {
+
+    /******************************************************************************************************************
+     ***************************************            Variables               ***************************************
+     *****************************************************************************************************************/
     enum Stage {
         // each number value represents number of cards the table should have
         PREFLOP(0), FLOP(3), TURN(4), RIVER(5);
@@ -24,6 +28,8 @@ public class Table {
         public Stage getNextStage(){
             return stageValues[(this.ordinal()+1) % 4];
         };
+
+        public static final int size = stageValues.length;
     }
 
     private Stage stage = Stage.PREFLOP;
@@ -34,23 +40,86 @@ public class Table {
     // pot holds the current round's bets
     private int pot = 0;
 
+    private boolean roundInProgress = false;
+
+    private final int maxUsers = 6;
+
     // Cards dealt onto table for all to use;
     private List<Card> communityCards = new ArrayList<>();
 
     private Deck deck = new Deck();
 
-    void dealToTable(){
+    private List<Player> players= new ArrayList<>();
+    /******************************************************************************************************************
+     ***************************************              Methods               ***************************************
+     *****************************************************************************************************************/
+    // TODO: uncomment when the User class is made
+//    public void nextTurn(){
+//        do {
+//            this.turn = (this.turn + 1) % (userList.size() + 1);
+//        } while (userList.get(i).didUserFold());
+//    }
+    // to use for testing until above function can be used
+    public void nextTurn() { turn+= (turn+1) % 2; }
+    int mainTurnID = 1; //to be used for debugging
+
+    public int getTurn(){ return turn; }
+
+    // TODO: add method to add users and start them in their own thread.
+//    public boolean addUser(){
+//        if (users.size > maxUsers)
+//            return false;
+//    }
+
+    private void startNewRound(){
+        pot = 0;
+        communityCards = new ArrayList<>();
+        roundInProgress = true;
+        deck.shuffle();
+        playRound();
+    }
+
+    // first table acts, then players play
+    private void playRound(){
+        // TODO:    add condition to loop checking that playing users > 1, maybe with static function in User class.
+        // This loop will go through each user's turn and dealing cards.
+        for (int i = 0; i < Stage.size; i++) {
+            switch (stage) {
+                case PREFLOP:
+                    //dealToPlayers():
+                    nextTurn();
+                    break;
+                case RIVER:
+                    dealToTable();
+                    nextTurn();
+                    break;
+                default:
+                    dealToTable();
+                    nextTurn();
+                    break;
+            }
+            // 0 represents table's turn.
+            // Maybe make static int in Player class called tableTurnID = 0 and use that instead?
+            while (getTurn() != 0);
+        }
+        // TODO: call a method here to figure out who won.
+        // calculateWinner();
+        roundInProgress = false;
+    }
+
+    private void dealToTable(){
         while (communityCards.size() < stage.getNumberOfCardsNeeded()){
-            communityCards.add(deck.deal());
+            communityCards.add(deck.dealOneCard());
         }
     }
 
+    /******************************************************************************************************************
+     ***************************************                Main                ***************************************
+     *****************************************************************************************************************/
+
     // currently used for testing
-    public static void main(String [ ] args){
+    public static void main(String [] args){
         Table table = new Table();
-        for (int i = 0; i < 4; i++) {
-            System.out.println(table.stage.toString());
-            table.stage = table.stage.getNextStage();
-        }
+
     }
 }
