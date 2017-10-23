fd5c505cba4af4402bb0b5f531432c0a0523e81a
Jorge Peralta <jperalta0789@gmail.com>
2017-10-23 02:47:12 -0400

    Suit does make more sense to be a part of the card class. Added javadoc comments to methods. Cleaned up class code.
    Signed-off-by: Jorge Peralta <jperalta0789@gmail.com>

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,25 +1,81 @@
 package com.company;
 
-import java.util.*;
-
 /**
  * Created by Ethan on 9/27/2017.
  */
 public class Card {
 
-    private int suit;
-    private int value;
+    /**
+     * Suit enum class preset with appropriate string and int values.
+     */
+    enum Suit {
 
-    public int getSuit(){
-        return suit;
+        DIAMONDS("Diamonds", 1), CLUBS("Clubs", 2), HEARTS("Hearts", 3), SPADES("Spades", 4);
+
+        private final String suitName;
+        private final Integer suitValue;
+
+        Suit(String suit, Integer suitValue) {
+            this.suitName = suit;
+            this.suitValue = suitValue;
+        }
+
+        public String getSuitName() {
+            return suitName;
+        }
+        public Integer getSuitValue() {
+            return suitValue;
+        }
     }
 
-    public int getValueValue(){
+    /**
+     * Card member variables.
+     */
+    private String suitName;
+    private int suitValue;
+    private int value;
+
+    /**
+     * Return suitName value.
+     *
+     * @return String
+     */
+    public String getSuitName(){
+        return suitName;
+    }
+
+    /**
+     * Return suitValue value.
+     *
+     * @return int
+     */
+    public int getSuitValue(){
+        return suitValue;
+    }
+
+    /**
+     * Return value value.
+     *
+     * @return int
+     */
+    public int getValue(){
         return value;
     }
 
-    Card(int suit, int value){
-        this.suit = suit;
+    /**
+     * Card constructor.
+     *
+     * @param suit Suit
+     * @param value int
+     */
+    Card(Suit suit, int value){
+        this.suitName = suit.getSuitName();
+        this.suitValue = suit.getSuitValue();
         this.value = value;
+    }
+
+    @Override
+    public String toString(){
+        return "The " + value + " of " + suitName + "\n";
     }
 }
