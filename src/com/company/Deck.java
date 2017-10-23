906823efdf42b14a8e4567f21bd3fe6b3b33be30
Jorge Peralta <jperalta0789@gmail.com>
2017-10-23 01:22:10 -0400

    Amended incorrect spelling of variable suits.
    Signed-off-by: Jorge Peralta <jperalta0789@gmail.com>

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -12,9 +12,9 @@ public class Deck {
 
     Deck(){
         init();
-    }
+	}
 
-    private static final int CARDS_PER_SUITE = 13;
+    private static final int CARDS_PER_SUITE  = 13;
     private static final int NUMBER_OF_SUITES = 4;
 
     private Stack<Card> cards;
@@ -22,31 +22,31 @@ public class Deck {
 
     private void init() {        //this is essentially the initializer since we only need the deck to be created once
         cards = new Stack<>();
-        for (int i = 0; i < NUMBER_OF_SUITES; i++) {
-            for (int j = 2; j < CARDS_PER_SUITE + 2; j++) {
-                cards.push(new Card(Card.Suite.values()[i], j));
+        for (int i = 0; i < NUMBER_OF_SUITES; i++){
+            for (int j = 2; j < CARDS_PER_SUITE + 2; j++){
+                cards.push(new Card(Suits.values()[i], j));
             }
         }
         usedCards = new Stack<>();
     }
-
+    
     public void shuffle() {
         // restoring used cards back to the deck
         while (!usedCards.isEmpty())
             cards.push(usedCards.pop());
-
+    	
         Collections.shuffle(cards);
     }
 
-    public Card dealOneCard() {    // we would have to call this twice when dealing
-        Card deal = cards.pop();    // what is dealt should have its own stack so we don't have to fillDeck every round
-        usedCards.push(deal);
-        return deal;
+    public Card dealOneCard(){	// we would have to call this twice when dealing
+    	Card deal = cards.pop();	// what is dealt should have its own stack so we don't have to fillDeck every round
+    	usedCards.push(deal);
+    	return deal;    	
     }
 
     public static void main(String[] args) {
         Deck deck = new Deck();
         deck.init();
         System.out.printf("ok");
-    }
+}
 }
