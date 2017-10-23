6c3459147001cd655020df32d3997df0a8f6de28
Ethan <ethanbinya@aol.com>
2017-10-23 12:48:26 -0400

    Misc changes, still unfinished.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,3 +1,4 @@
+package com.company;
 import java.util.*;
 
 /**
@@ -7,38 +8,40 @@ import java.util.*;
 public class Player {
 
 	private String username;
+	private int turnID;
 	private int balance;
 	private boolean smallBlind;
 	private boolean bigBlind;
 	private boolean canPlay;
-	
+
 	Player(String username, int balance){
 		this.username = username;
 		this.balance = balance;
 	}
-	
+
 	public void display() {
 		//should display their name, balance, and two cards
 		//might also be displaying their possible actions when its their turn?
 	}
-	
-	public boolean isDealer() {
-		
-	}
-	
+
+//	public boolean isDealer() {
+//
+//	}
+
 	public boolean isSmallBlind() {
 		//we need a counter to know which player its up to to determine this, bigBlind and Dealer too
 		//we also need a player ID to be able to do ^^^^^^
+		return smallBlind;
 	}
-	
+
 	public boolean isBigBlind() {
-		
+		return bigBlind;
 	}
-	
+
 	public int getBalance() {
 		return balance;
 	}
-	
+
 	public boolean canPlay() {
 		return balance > 0;
 	}
