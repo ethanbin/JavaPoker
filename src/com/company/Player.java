5fb2c5d213119f4869478aacfa9d5bf8d9df94d8
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-10-24 16:09:18 -0400

    Update Player.java
    Take a look at main... 
    Yes, it displays what we want to see, but when I try to get a specifically call it for a certain player, it's wrong.
    I also tried using a setter but it didn't seem to help so I got rid of it.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -8,13 +8,14 @@ import java.util.*;
 public class Player {
 
 	private String username;
-	private int turnID;
 	private int balance;
 	private boolean smallBlind;
 	private boolean bigBlind;
 	private boolean canPlay;
+	private static int playerID = 0;
 
 	Player(String username, int balance){
+		playerID = getPlayerID();
 		this.username = username;
 		this.balance = balance;
 	}
@@ -43,6 +44,29 @@ public class Player {
 	}
 
 	public boolean canPlay() {
-		return balance > 0;
+		return getBalance() > 0;
+	}
+	
+	public int getPlayerID() {
+		playerID++;
+		return playerID;
+	}
+	
+	@Override
+	public String toString() {
+		return "Player [username=" + username + ", balance=" + getBalance() + ", canPlay=" + canPlay() + ", PlayerID="
+				+ playerID + "]";
+	}
+	
+	public static void main(String [] args) {
+		Player k = new Player("Kay", 2000);
+		System.out.println(k.toString());
+		Player l = new Player("Rob", -1000);
+		System.out.println(l.toString());
+		Player m = new Player("Vanessa", 0);
+		System.out.println(m.toString());
+		Player n = new Player("David", 251);
+		System.out.println(n.toString());
+		System.out.println(k.getPlayerID());
 	}
 }
