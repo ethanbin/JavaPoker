cf0a2f8d5bbdd1d41aefb2ee4a202b606b95ef10
Jorge Peralta <jperalta0789@gmail.com>
2017-10-30 01:35:21 -0400

    Player class does not need to worry about what stage it is in: dealer, small blind, or big blind.
    Added a constructor with a default player balance value.
    
    Signed-off-by: Jorge Peralta <jperalta0789@gmail.com>

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -8,19 +8,29 @@ import java.util.*;
 
 public class Player {
 
+    private final int DEFAULT_PLAYER_BALANCE = 2000;
+
     private String username;
     private int balance;
-    private boolean smallBlind;
-    private boolean bigBlind;
     private Stack<Card> holeCards;
+
+    Player(String username) {
+        this.username = username;
+        this.balance = DEFAULT_PLAYER_BALANCE;
+        this.init();
+    }
 
     Player(String username, int balance) {
         this.username = username;
         this.balance = balance;
+        this.init();
+    }
+
+    private void init(){
         this.holeCards = new Stack<>();
     }
 
-    public void displayPlayerInfo() {
+    protected void displayPlayerInfo() {
         System.out.println(this);
         System.out.println(username+"'s cards:");
         for (Card card : holeCards) {
@@ -29,30 +39,20 @@ public class Player {
         System.out.println("\n");
     }
 
-//	public boolean isDealer() {
-//
-//	}
-
-    public boolean isSmallBlind() {
-        //we need a counter to know which player its up to to determine this, bigBlind and Dealer too
-        //we also need a player ID to be able to do ^^^^^^
-        return smallBlind;
-    }
-
-    public boolean isBigBlind() {
-        return bigBlind;
-    }
-
-    public int getBalance() {
+    protected int getBalance() {
         return balance;
     }
 
-    public boolean canPlay() {
+    protected boolean canPlay() {
         return getBalance() > 0;
     }
 
-    public void givePlayerCard(Card card) {
+    protected void givePlayerCard(Card card) {
         this.holeCards.push(card);
+    }
+
+    protected String getUserName(){
+        return username;
     }
 
     @Override
@@ -74,7 +74,7 @@ public class Player {
 
         Stack<Player> players = new Stack<>();
 
-        players.push(new Player("Kay", 2000));
+        players.push(new Player("Kay"));
         players.push(new Player("Rob", -1000));
         players.push(new Player("Vanessa", 0));
         players.push(new Player("David", 251));
