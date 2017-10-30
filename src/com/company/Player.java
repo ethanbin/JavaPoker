6fef8503ca13f26d73706b55f2e61ec277f0521a
Jorge Peralta <jperalta0789@gmail.com>
2017-10-29 23:45:06 -0400

    We can now assign cards to Player. Main simulates Table logic of dealing cards to each Player.
    Signed-off-by: Jorge Peralta <jperalta0789@gmail.com>

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.*;
 
 /**
@@ -7,57 +8,91 @@ import java.util.*;
 
 public class Player {
 
-	private String username;
-	private int balance;
-	private boolean smallBlind;
-	private boolean bigBlind;
-	private boolean canPlay;
+    private String username;
+    private int balance;
+    private boolean smallBlind;
+    private boolean bigBlind;
+    private Stack<Card> playerCards;
 
-	Player(String username, int balance){
-		this.username = username;
-		this.balance = balance;
-	}
+    Player(String username, int balance) {
+        this.username = username;
+        this.balance = balance;
+        this.playerCards = new Stack<>();
+    }
 
-	public void displayPlayerInfo() {
-		//should displayPlayerInfo their name, balance, and two cards
-		//might also be displaying their possible action when its their turn?
-	}
+    public void displayPlayerInfo() {
+        System.out.println(this);
+        System.out.println(username+"'s cards:");
+        for (Card card : playerCards) {
+            System.out.print(card);
+        }
+        System.out.println("\n");
+    }
 
 //	public boolean isDealer() {
 //
 //	}
 
-	public boolean isSmallBlind() {
-		//we need a counter to know which player its up to to determine this, bigBlind and Dealer too
-		//we also need a player ID to be able to do ^^^^^^
-		return smallBlind;
-	}
+    public boolean isSmallBlind() {
+        //we need a counter to know which player its up to to determine this, bigBlind and Dealer too
+        //we also need a player ID to be able to do ^^^^^^
+        return smallBlind;
+    }
 
-	public boolean isBigBlind() {
-		return bigBlind;
-	}
+    public boolean isBigBlind() {
+        return bigBlind;
+    }
 
-	public int getBalance() {
-		return balance;
-	}
+    public int getBalance() {
+        return balance;
+    }
 
-	public boolean canPlay() {
-		return getBalance() > 0;
-	}
-	
-	@Override
-	public String toString() {
-		return "Player [username=" + username + ", balance=" + getBalance() + ", canPlay=" + canPlay() + "]";
-	}
-	
-	public static void main(String [] args) {
-		Player k = new Player("Kay", 2000);
-		System.out.println(k.toString());
-		Player l = new Player("Rob", -1000);
-		System.out.println(l.toString());
-		Player m = new Player("Vanessa", 0);
-		System.out.println(m.toString());
-		Player n = new Player("David", 251);
-		System.out.println(n.toString());
-	}
+    public boolean canPlay() {
+        return getBalance() > 0;
+    }
+
+    public void givePlayerCard(Card card) {
+        this.playerCards.push(card);
+    }
+
+    @Override
+    public String toString() {
+        String playerInfo;
+
+        playerInfo = "username = " + username + "\n";
+        playerInfo += username + "'s Balance is $" + getBalance() + "\n";
+        if(canPlay()){
+            playerInfo += username + " is still able to play.";
+        } else {
+            playerInfo += username + " is no longer able to play.";
+        }
+        return playerInfo;
+    }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+
+        Stack<Player> players = new Stack<>();
+
+        players.push(new Player("Kay", 2000));
+        players.push(new Player("Rob", -1000));
+        players.push(new Player("Vanessa", 0));
+        players.push(new Player("David", 251));
+
+        System.out.println("Dealing Cards To Players...\n");
+
+        for (Player player : players) {
+            player.givePlayerCard(deck.dealCard());
+        }
+        for (Player player : players) {
+            player.givePlayerCard(deck.dealCard());
+        }
+
+        System.out.println("Players Information...\n");
+
+        for (Player player : players) {
+            player.displayPlayerInfo();
+        }
+        
+    }
 }
